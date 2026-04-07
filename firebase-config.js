e795a428758c89a2618bc977d9d150ee8bda02a6
chlgkrud981109 <chlgkrud981109@github.com>
2026-04-08 00:33:47 +0900

    UI 새로 시작

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBEkQmeFG8XGxNuYDZof_3_9oOS4cEMOyk",
+  authDomain: "story-vital.firebaseapp.com",
+  projectId: "story-vital",
+  storageBucket: "story-vital.firebasestorage.app",
+  messagingSenderId: "872952700994",
+  appId: "1:872952700994:web:efa41cd38cd491fe5ccddb"
+};
